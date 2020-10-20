7f53bb8a2264ba413ba6fc9a9c42cdc1b9c20dcc
craiglandis <craiglandis@users.noreply.github.com>
2020-10-20 12:10:50 -0700

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -4,17 +4,18 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 
 reg add "HKCU\SOFTWARE\Microsoft\ServerManager" /v DoNotOpenServerManagerAtLogon /t REG_DWORD /d 1 /f
 
-cinst 7zip.install -y
-cinst notepadplusplus.install -y
-#cinst microsoft-edge -y
-#cinst googlechrome -y
+choco install 7zip.install -y
+choco install notepadplusplus.install -y
+#choco install microsoft-edge -y
+#choco install googlechrome -y
 #Chocolatey install of sysinternals is slow, will download zip instead
-#cinst sysinternals -y
-cinst windirstat -y
-cinst microsoft-windows-terminal -y
-cinst autohotkey -y
-cinst powershell-core -y
-cinst azcopy10 -y
+#choco install sysinternals -y
+#cuninst sysinternals -y
+choco install windirstat -y
+choco install microsoft-windows-terminal -y
+choco install autohotkey -y
+choco install powershell-core -y
+choco install azcopy10 -y
 
 $uri = "http://live.sysinternals.com/Files/SysinternalsSuite.zip"
 $myPath = "$env:SystemDrive\my"
@@ -24,34 +25,34 @@ Invoke-WebRequest -UseBasicParsing -Uri $uri -OutFile $outFile -Verbose
 Expand-Archive -LiteralPath $outFile -DestinationPath $myPath -Force
 
 <#
-cinst crystaldiskmark -y
-cinst nircmd -y
-cinst iperf3 -y
-cinst windbg -y
-cinst gpu-z -y
-cinst screentogif -y
-cinst speccy -y
-cinst beyondcompare -y
-cinst nmap -y
-cinst tightvnc -y
-cinst cpu-z.install -y
-cinst dotnetcore-runtime -y
-cinst microsoft-edge -y
-cinst postman -y
-cinst fiddler -y
-cinst wireshark -y
-cinst winscp.install -y
-cinst treesizefree -y
-cinst az.powershell -y
-cinst azure-cli -y
-cinst git.install -y
-cinst microsoftazurestorageexplorer -y
-cinst vscode -y
-cinst python -y
-cinst greenshot -y
-cinst powershell -y # WMF+PS5.1
-cinst putty.install -y
-cinst dotnetfx -y # .NET Framework 4.8
+choco install crystaldiskmark -y
+choco install nircmd -y
+choco install iperf3 -y
+choco install windbg -y
+choco install gpu-z -y
+choco install screentogif -y
+choco install speccy -y
+choco install beyondcompare -y
+choco install nmap -y
+choco install tightvnc -y
+choco install cpu-z.install -y
+choco install dotnetcore-runtime -y
+choco install microsoft-edge -y
+choco install postman -y
+choco install fiddler -y
+choco install wireshark -y
+choco install winscp.install -y
+choco install treesizefree -y
+choco install az.powershell -y
+choco install azure-cli -y
+choco install git.install -y
+choco install microsoftazurestorageexplorer -y
+choco install vscode -y
+choco install python -y
+choco install greenshot -y
+choco install powershell -y # WMF+PS5.1
+choco install putty.install -y
+choco install dotnetfx -y # .NET Framework 4.8
 
 $exeUri = 'https://download.microsoft.com/download/B/E/1/BE1F235A-836D-42AC-9BC1-8F04C9DA7E9D/vc_uwpdesktop.140.exe'
 $exeName = $exeUri.Split('/')[-1]
