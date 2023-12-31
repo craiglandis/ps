164975fbbbfa0a560582758db5e5e6ad59b8e304
craiglandis <craiglandis@users.noreply.github.com>
2023-12-31 05:16:29 -0800

    Update Get-AuthenticodeSignatureIssuers.ps1

--- Get-AuthenticodeSignatureIssuers.ps1
+++ Get-AuthenticodeSignatureIssuers.ps1
@@ -1,19 +1,21 @@
-# Get-AuthenticodeSignatureIssuers.ps1
 param(
     [string]$processName = '*'
 )
 
 $issuers = New-Object System.Collections.Generic.List[String]
 
-$modules = Get-Process -Name $processName | Select-Object -ExpandProperty Modules
-$moduleFilePaths = $modules | Sort-Object -Unique FileName | Select-Object -ExpandProperty FileName
+$modules = Get-Process -Name $processName | Select-Object -ExpandProperty Modules -ErrorAction SilentlyContinue
+$moduleFilePaths = $modules | Sort-Object -Unique FileName | Select-Object -ExpandProperty FileName -ErrorAction SilentlyContinue
 
 foreach ($filePath in $moduleFilePaths)
 {
     # $signature = Get-AuthenticodeSignature C:\MODULES\Az.Accounts\2.13.2\FuzzySharp.dll
     $signature = Get-AuthenticodeSignature -FilePath $filePath -ErrorAction SilentlyContinue
     $issuer = $signature.SignerCertificate.Issuer
-    $issuers.Add($issuer)
+    if ([string]::IsNullOrEmpty($issuer) -eq $false)
+    {
+        $issuers.Add($issuer)
+    }
 }
 $issuers = $issuers | Sort-Object -Unique
 $issuersCount = $issuers | Measure-Object | Select-Object -ExpandProperty Count
