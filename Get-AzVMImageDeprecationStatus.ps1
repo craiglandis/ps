1a30bef97e63c4d091f6c43fa09577f707dfce59
craiglandis <craiglandis@users.noreply.github.com>
2024-08-29 16:38:16 -0700

    Update Get-AzVMImageDeprecationStatus.ps1

--- Get-AzVMImageDeprecationStatus.ps1
+++ Get-AzVMImageDeprecationStatus.ps1
@@ -2,17 +2,28 @@ $vms = Get-AzVM
 
 foreach ($vm in $vms)
 {
-    $image = Get-AzVmImage -Location $vm.location -PublisherName $vm.StorageProfile.ImageReference.Publisher -Offer $vm.StorageProfile.ImageReference.Offer -Skus $vm.StorageProfile.ImageReference.Sku -Version $vm.StorageProfile.ImageReference.ExactVersion
+    $publisher = $vm.StorageProfile.ImageReference.Publisher
+    $offer = $vm.StorageProfile.ImageReference.Offer
+    $sku = $vm.StorageProfile.ImageReference.Sku
+    $exactVersion = $vm.StorageProfile.ImageReference.ExactVersion
+    $urn = "$($publisher):$($offer):$($sku):$($exactVersion)"
+
+    $image = Get-AzVMImage -Location $vm.location -PublisherName $vm.StorageProfile.ImageReference.Publisher -Offer $vm.StorageProfile.ImageReference.Offer -Skus $vm.StorageProfile.ImageReference.Sku -Version $vm.StorageProfile.ImageReference.ExactVersion
 
     $imageDeprecationStatus = $image.ImageDeprecationStatus
 
     $vm.StorageProfile.ImageReference | Add-Member -MemberType NoteProperty -Name ImageState -Value $image.ImageDeprecationStatus.ImageState -Force
     $vm.StorageProfile.ImageReference | Add-Member -MemberType NoteProperty -Name ScheduledDeprecationTime -Value $image.ImageDeprecationStatus.ScheduledDeprecationTime -Force
     $vm.StorageProfile.ImageReference | Add-Member -MemberType NoteProperty -Name AlternativeOption -Value $image.ImageDeprecationStatus.AlternativeOption -Force
+    $vm.StorageProfile.ImageReference | Add-Member -MemberType NoteProperty -Name ImageUrn -Value $urn -Force
 }
 
-$imageState = @{Name = 'ImageState'; Expression={$_.StorageProfile.ImageReference.ImageState}}
-$scheduledDeprecationTime = @{Name = 'ScheduledDeprecationTime'; Expression={$_.StorageProfile.ImageReference.ScheduledDeprecationTime}}
-$alternativeOption = @{Name = 'AlternativeOption'; Expression={$_.StorageProfile.ImageReference.AlternativeOption}}
 
-$vms | Format-Table Name,ResourceGroupName,$imageState,$scheduledDeprecationTime,$alternativeOption -AutoSize
+$vmName = @{Name = 'VM'; Expression = {$_.Name}}
+$rgName = @{Name = 'RG'; Expression = {$_.ResourceGroupName}}
+$imageState = @{Name = 'ImageState'; Expression = {$_.StorageProfile.ImageReference.ImageState}}
+$scheduledDeprecationTime = @{Name = 'ScheduledDeprecationTime'; Expression = {$_.StorageProfile.ImageReference.ScheduledDeprecationTime}}
+$alternativeOption = @{Name = 'AlternativeOption'; Expression = {$_.StorageProfile.ImageReference.AlternativeOption}}
+$imageUrn = @{Name = 'ImageUrn'; Expression = {$_.StorageProfile.ImageReference.ImageUrn}}
+
+$vms | Format-Table $vmName, $rgName, $imageUrn, $imageState, $scheduledDeprecationTime, $alternativeOption -AutoSize
