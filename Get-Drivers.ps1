6a2568eafeb9d1537cd815c4b2a0e31f76382543
craiglandis <craiglandis@users.noreply.github.com>
2024-12-10 12:50:28 -0800

    Update Get-Drivers.ps1

--- Get-Drivers.ps1
+++ Get-Drivers.ps1
@@ -5,6 +5,207 @@ param(
     [switch]$show
 )
 
+function Out-Log
+{
+    param(
+        [string]$text,
+        [switch]$verboseOnly,
+        [switch]$startLine,
+        [switch]$endLine,
+        [switch]$raw,
+        [switch]$logonly,
+        [ValidateSet('Black','Blue','Cyan','DarkBlue','DarkCyan','DarkGray','DarkGreen','DarkMagenta','DarkRed','DarkYellow','Gray','Green','Magenta','Red','White','Yellow')]
+        [string]$color = 'White'
+    )
+
+    $utc = (Get-Date).ToUniversalTime()
+
+    $logTimestampFormat = 'yyyy-MM-dd hh:mm:ssZ'
+    $logTimestampString = Get-Date -Date $utc -Format $logTimestampFormat
+
+    if ([string]::IsNullOrEmpty($script:scriptStartTimeUtc))
+    {
+        $script:scriptStartTimeUtc = $utc
+        $script:scriptStartTimeUtcString = Get-Date -Date $utc -Format $logTimestampFormat
+    }
+
+    if ([string]::IsNullOrEmpty($script:lastCallTime))
+    {
+        $script:lastCallTime = $utc
+    }
+
+    $lastCallTimeSpan = New-TimeSpan -Start $script:lastCallTime -End $utc
+    $lastCallTotalSeconds = $lastCallTimeSpan | Select-Object -ExpandProperty TotalSeconds
+    $lastCallTimeSpanFormat = '{0:ss}.{0:ff}'
+    $lastCallTimeSpanString = $lastCallTimeSpanFormat -f $lastCallTimeSpan
+    $lastCallTimeSpanString = "$($lastCallTimeSpanString)s"
+    $script:lastCallTime = $utc
+
+    if ($verboseOnly)
+    {
+        $callstack = Get-PSCallStack
+        $caller = $callstack | Select-Object -First 1 -Skip 1
+        $caller = $caller.InvocationInfo.MyCommand.Name
+        if ($caller -eq 'Invoke-ExpressionWithLogging')
+        {
+            $caller = $callstack | Select-Object -First 1 -Skip 2
+            $caller = $caller.InvocationInfo.MyCommand.Name
+        }
+
+        if ($verbose)
+        {
+            $outputNeeded = $true
+        }
+        else
+        {
+            $outputNeeded = $false
+        }
+    }
+    else
+    {
+        $outputNeeded = $true
+    }
+
+    if ($outputNeeded)
+    {
+        if ($raw -or $global:helperOutputRaw)
+        {
+            if ($logonly)
+            {
+                if ($logFilePath)
+                {
+                    $text | Out-File $logFilePath -Append
+                }
+            }
+            else
+            {
+                Write-Host $text -ForegroundColor $color
+                if ($logFilePath)
+                {
+                    $text | Out-File $logFilePath -Append
+                }
+            }
+        }
+        else
+        {
+            $timespan = New-TimeSpan -Start $script:scriptStartTimeUtc -End $utc
+
+            $timespanFormat = '{0:mm}:{0:ss}'
+            $timespanString = $timespanFormat -f $timespan
+
+            $consolePrefixString = "$timespanString "
+            $logPrefixString = "$logTimestampString $timespanString $lastCallTimeSpanString"
+
+            if ($logonly -or $global:quiet)
+            {
+                if ($logFilePath)
+                {
+                    "$logPrefixString $text" | Out-File $logFilePath -Append
+                }
+            }
+            else
+            {
+                if ($verboseOnly)
+                {
+                    $consolePrefixString = "$consolePrefixString[$caller] "
+                    $logPrefixString = "$logPrefixString[$caller] "
+                }
+
+                if ($startLine)
+                {
+                    $script:startLineText = $text
+                    Write-Host $consolePrefixString -NoNewline -ForegroundColor DarkGray
+                    Write-Host "$text " -NoNewline -ForegroundColor $color
+                }
+                elseif ($endLine)
+                {
+                    Write-Host $text -ForegroundColor $color
+                    if ($logFilePath)
+                    {
+                        "$logPrefixString $script:startLineText $text" | Out-File $logFilePath -Append
+                    }
+                }
+                else
+                {
+                    Write-Host $consolePrefixString  -NoNewline -ForegroundColor DarkGray
+                    Write-Host $text -ForegroundColor $color
+                    if ($logFilePath)
+                    {
+                        "$logPrefixString $text" | Out-File $logFilePath -Append
+                    }
+                }
+            }
+        }
+    }
+}
+
+function Invoke-ExpressionWithLogging
+{
+    param(
+        [string]$command,
+        [switch]$raw,
+        [switch]$verboseOnly,
+        [switch]$commandWhatIf
+    )
+
+    # This results in error:
+    # Cannot convert argument "newChar", with value: "", for "Replace" to type "System.Char": "Cannot convert value "" to
+    # type "System.Char". Error: "String must be exactly one character long.""
+    # $command = $command.Replace($green, '').Replace($reset, '')
+
+    # Write-Host "[HELPER] `$global:whatif: $global:whatif" -ForegroundColor Yellow
+    # Write-Host "[HELPER] `$script:whatif: $script:whatif" -ForegroundColor Yellow
+    # Write-Host "[HELPER] `$commandWhatIf: $commandWhatIf" -ForegroundColor Cyan
+    # if ($global:whatif -and $commandWhatIf)
+    if ($commandWhatIf)
+    {
+        Out-Log "[WHATIF] $command" -color Cyan
+    }
+    else
+    {
+        if ($verboseOnly)
+        {
+            if ($verbose)
+            {
+                if ($raw -or $global:helperOutputRaw)
+                {
+                    Out-Log $command -verboseOnly -raw
+                }
+                else
+                {
+                    Out-Log $command -verboseOnly
+                }
+            }
+        }
+        else
+        {
+            if ($raw -or $global:helperOutputRaw)
+            {
+                Out-Log $command -raw
+            }
+            else
+            {
+                Out-Log $command
+            }
+        }
+
+        try
+        {
+            Invoke-Expression -Command $command
+        }
+        catch
+        {
+            $global:errorRecordObject = $PSItem
+            Out-Log "`n$command`n" -raw -color Red
+            Out-Log "$global:errorRecordObject" -raw -color Red
+            if ($LASTEXITCODE)
+            {
+                Out-Log "`$LASTEXITCODE: $LASTEXITCODE`n" -raw -color Red
+            }
+        }
+    }
+}
+
 $microsoftIssuers = @'
 CN=Microsoft Code Signing PCA 2010, O=Microsoft Corporation, L=Redmond, S=Washington, C=US
 CN=Microsoft Code Signing PCA 2011, O=Microsoft Corporation, L=Redmond, S=Washington, C=US
